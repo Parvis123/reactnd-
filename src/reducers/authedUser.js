11340a19a3c1a586ce7061fffa18b8bd97135dde
Michael <michael_parvis@hotmail.com>
2020-10-20 20:07:50 +0100

    set state authedUser reducer to null to stop error

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,6 +1,6 @@
 import { SET_AUTHED_USER, DELETE_AUTHED_USER } from "../actions/authedUser";
 
-export default function (state, action) {
+export default function (state = null, action) {
   switch (action.type) {
     case SET_AUTHED_USER:
       return action.id;
